18715666883f1cb22235a06687727b320c98beaa
Alec Faustino <alecfaustino@outlook.com>
2025-02-21 07:10:20 -0700

    add more test cases

--- tail.js
+++ tail.js
@@ -14,3 +14,10 @@ const tail = (arr) => {
 
 assertEqual(tail([5, 6, 7]).length, 2);
 assertEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse");
+const testArr = ["Test"]
+assertEqual(tail(testArr).length, 0);
+const mutableTest = [1, 2, 3 ,4];
+const mutated = tail(mutableTest);
+assertEqual(mutableTest.length, 4);
+assertEqual(mutated.length, 3);
+

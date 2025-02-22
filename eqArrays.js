ab0ed5173f38c7af91c6dcf87db40fe73856b427
Alec Faustino <alecfaustino@outlook.com>
2025-02-22 11:35:45 -0700

    add EqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+
+  if (actual !== expected) {
+    return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([1, 2, 3], [ "1", "2", "3"]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+assertEqual(eqArrays(["Name1", "Name2", 3], ["Name1", "Name2", 3]), true);
+

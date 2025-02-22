f46c9404a3c0a54c7e657179261ca8ea65b5fded
Alec Faustino <alecfaustino@outlook.com>
2025-02-22 11:41:40 -0700

    refactor assertEqual in all files and linted all files

--- head.js
+++ head.js
@@ -2,15 +2,13 @@ const assertEqual = (actual, expected) => {
   if (actual === expected) {
     return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
-
-  if (actual !== expected) {
-    return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
+  return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
+
 
 const head = (arr) => {
   return arr[0];
-}
+};
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

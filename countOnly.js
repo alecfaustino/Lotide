05583a538cd87e7b2e2b6c9d6f06d77d65ea5d89
Alec Faustino <alecfaustino@outlook.com>
2025-02-23 16:54:20 -0700

    add countOnly Function

---
+++ countOnly.js
@@ -0,0 +1,50 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//allItems is the array of strings that we need to look through
+//itemsToCount is an object specifying what to count.
+const countOnly = (allItems, itemsToCount) => {
+  const results = {};
+  for (const item of allItems) {
+    // if I don't add the &&, it asserts the last test as fail because it DOES have that name, but we don't want to count it. We only want to count it if it is true.
+    // original code was if (itemsToCount.hasOwnProperty(item) && (itemsToCount[item])) { but eslint asks to use this Object.prototype to make it more safe -- will have to ask why.. it still doesn't make sense
+    if (Object.prototype.hasOwnProperty.call(itemsToCount, item) && (itemsToCount[item])) {
+      // if the item is not yet in the results object, start the count at 1
+      if (!results[item]) {
+        results[item] = 1;
+      } else {
+        // if it is already in the object, increment by 1.
+        results[item] ++;
+      }
+    }
+  }
+  return results;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe",
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true,
+  Agouhanna: false,
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);

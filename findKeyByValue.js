60db2ba0dab459181d8988e31b90b507549d5005
Alec Faustino <alecfaustino@Alecs-MacBook-Pro.local>
2025-02-24 07:16:08 -0700

    Add findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = (obj, value) => {
+  for (const keys in obj) {
+    if (obj[keys] === value) {
+      return keys;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

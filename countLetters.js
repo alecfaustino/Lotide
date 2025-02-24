bb2382a511345c50882cdb0f37d90df37bf08de3
Alec Faustino <alecfaustino@Alecs-MacBook-Pro.local>
2025-02-23 17:25:51 -0700

    declared const in for loop and linted the file

--- countLetters.js
+++ countLetters.js
@@ -6,11 +6,11 @@ const assertEqual = (actual, expected) => {
 };
 
 const countLetters = (sentence) => {
-  let counts = {}
+  let counts = {};
   //Without this, it counts " " as a letter and creates a key for it
   let sentenceNoSpace = sentence.split(" ").join("");
-  for (letter of sentenceNoSpace) {
-    if(!counts[letter]) {
+  for (const letter of sentenceNoSpace) {
+    if (!counts[letter]) {
       counts[letter] = 1;
     } else {
       counts[letter] ++;
@@ -20,7 +20,7 @@ const countLetters = (sentence) => {
 };
 
 const resultTest1 = countLetters("alec is learning in 2025");
-assertEqual(resultTest1["a"], 2 );
+assertEqual(resultTest1["a"], 2);
 assertEqual(resultTest1["2"], 2);
 assertEqual(resultTest1["g"], 1);
 assertEqual(resultTest1["z"], undefined);

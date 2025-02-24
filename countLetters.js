f6928cb3ae012f2945d306ec688228f8672c58f3
Alec Faustino <alecfaustino@Alecs-MacBook-Pro.local>
2025-02-23 17:20:49 -0700

    add countLetters function

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+  return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = (sentence) => {
+  let counts = {}
+  //Without this, it counts " " as a letter and creates a key for it
+  let sentenceNoSpace = sentence.split(" ").join("");
+  for (letter of sentenceNoSpace) {
+    if(!counts[letter]) {
+      counts[letter] = 1;
+    } else {
+      counts[letter] ++;
+    }
+  }
+  return counts;
+};
+
+const resultTest1 = countLetters("alec is learning in 2025");
+assertEqual(resultTest1["a"], 2 );
+assertEqual(resultTest1["2"], 2);
+assertEqual(resultTest1["g"], 1);
+assertEqual(resultTest1["z"], undefined);

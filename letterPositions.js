b30c138bee56defdd21a3f78ebad9607a528538b
Alec Faustino <alecfaustino@outlook.com>
2025-02-23 18:05:28 -0700

    add letter positions function

---
+++ letterPositions.js
@@ -0,0 +1,39 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+    return;
+  }
+  console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  
+};
+
+
+const letterPositions = (sentence) => {
+  let positions = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (!positions[sentence[i]]) {
+        positions[sentence[i]] = [i];
+      } else {
+        positions[sentence[i]].push(i);
+      }
+    }
+  }
+  return positions;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("where is the letter z").z, [20]);

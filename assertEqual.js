f9b415c970373a2cb2794596b55d898c7b512120
Alec Faustino <alecfaustino@outlook.com>
2025-03-05 19:36:55 -0700

    refactor head and assertEqual by exporting and creating separate test files

--- assertEqual.js
+++ assertEqual.js
@@ -5,9 +5,4 @@ const assertEqual = (actual, expected) => {
   return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("match", "match");
-assertEqual("1", 1);
-assertEqual(1, 2);
-
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,17 +1,5 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-};
-
-
 const head = (arr) => {
   return arr[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([1]), 1);
-assertEqual(head(["1"]), 1);
+module.exports = head;

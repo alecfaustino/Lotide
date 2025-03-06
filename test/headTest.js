c2b3235eccf13298f281a36cb2934a4a22e74f9b
Alec Faustino <alecfaustino@outlook.com>
2025-03-05 21:39:10 -0700

    add mocha and chai npm and refactor tests for head

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,42 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const head = require('../head');
 
-assertEqual(head([5, 6, 7]), 5); // should pass
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"); // should pass
-assertEqual(head([]), undefined); // should pass
-assertEqual(head([1]), 1); // should pass
-assertEqual(head(["1"]), 1); // should not pass
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    const input = [1, 2, 3];
+    const expectedOutput = 1;
+    assert.strictEqual(head(input), expectedOutput);
+  });
+
+  it("returns '5' for ['5']", () => {
+    const input = ['5'];
+    const expectedOutput = '5';
+    assert.strictEqual(head(input), expectedOutput); 
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    const input = [5, 6, 7];
+    const expectedOutput = 5;
+    assert.strictEqual(head(input), expectedOutput);
+  });
+
+  it('returns "Hello" for ["Hello", "Lighthouse", "Labs"]', () => {
+    const input = ["Hello", "Lighthouse", "Labs"];
+    const expectedOutput = "Hello";
+    assert.strictEqual(head(input), expectedOutput);
+  });
+
+  it("returns undefined for []", () => {
+    const input = [];
+    const expectedOutput = undefined;
+    assert.strictEqual(head(input), expectedOutput);
+  });
+
+  it("returns 1 for [1]", () => {
+    const input = [1];
+    const expectedOutput = 1;
+    assert.strictEqual(head(input), expectedOutput);
+  });
+
+
+});

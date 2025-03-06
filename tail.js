26b0dedc381b1d6e6f03a04e635cdc92269c22b2
Alec Faustino <alecfaustino@outlook.com>
2025-03-05 19:48:15 -0700

    refactor tail and add tailTest for testing code.

--- tail.js
+++ tail.js
@@ -1,20 +1,5 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  }
-  return console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const tail = (arr) => {
   return arr.slice(1);
 };
 
-assertEqual(tail([5, 6, 7]).length, 2);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"])[0], "Lighthouse");
-const testArr = ["Test"];
-assertEqual(tail(testArr).length, 0);
-const mutableTest = [1, 2, 3 ,4];
-const mutated = tail(mutableTest);
-assertEqual(mutableTest.length, 4);
-assertEqual(mutated.length, 3);
-
+module.exports = tail;
